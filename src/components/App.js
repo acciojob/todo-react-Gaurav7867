26c46818f6db90f619a9bf959eceacace8f7d032
Gaurav7867 <112535303+Gaurav7867@users.noreply.github.com>
2024-03-20 15:49:24 +0000

    final update

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 
 import React from "react";
 import './../styles/App.css';
+import TodoApp from "../todo";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+        {TodoApp}
     </div>
   )
 }
